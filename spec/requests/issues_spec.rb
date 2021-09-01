d4d6c31a7fa8add3f7d34c934d4cbcaa54026986
Lucasdfg07 <lucassiqueirafernandes07@gmail.com>
2021-09-01 15:34:17 -0300

    Refactoring tests syntax

--- spec/requests/issues_spec.rb
+++ spec/requests/issues_spec.rb
@@ -4,23 +4,29 @@ RSpec.describe "Issues", type: :request do
   
   describe "GET /api/v1/issues" do
     
-		it "returns success status" do
-			get '/api/v1/issues'
-			expect(response).to have_http_status(200)
+		context "when get Issues" do
+			it {
+        get '/api/v1/issues'
+        expect(response).to have_http_status(200) 
+      }
 		end
 
-		it "return issue on correct latitude and longitude" do
+		context "when get correct latitude and longitude" do
 			issue = FactoryBot.create(:issue)
-      get "/api/v1/issues?latitude=#{issue.latitude}&longitude=#{issue.longitude}"
-
-      expect(response.body).to include(issue.to_json)
+      
+      it { 
+        get "/api/v1/issues?latitude=#{issue.latitude}&longitude=#{issue.longitude}"
+        expect(response.body).to include(issue.to_json) 
+      }
     end
     
-    it "return empty array when params are not present" do
+    context "when dont get correct latitude and longitude" do
 			issue = FactoryBot.create(:issue)
-      get "/api/v1/issues?latitude=#{FFaker::Geolocation.lat}&longitude=#{FFaker::Geolocation.lng}"
-
-      expect(response.body).to eq([].to_json)
+      
+      it { 
+        get "/api/v1/issues?latitude=#{FFaker::Geolocation.lat}&longitude=#{FFaker::Geolocation.lng}"
+        expect(response.body).to eq([].to_json) 
+      }
 		end
 	end
 end
